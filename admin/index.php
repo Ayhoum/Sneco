8ef0f3f00edf1659c024cca2f6c89f9fe584b4db
Ayham <aylosa@outlook.com>
2017-06-02 16:58:05 +0200

    .

--- admin/index.php
+++ admin/index.php
@@ -19,26 +19,64 @@ if(isset($_POST['login-form-submit']))
     }
 }
 ?>
-<div class="tab-container">
-    <div class="tab-content clearfix" id="tab-login">
-        <div class="panel panel-default nobottommargin">
-            <div class="panel-body" style="padding: 40px;">
-                <form id="login-form" name="login-form" class="nobottommargin" action="index.php" method="post">
-                    <h3>Login to your Account</h3>
-                    <div class="col_full">
-                        <label for="login-form-username">Username:</label>
-                        <input type="text" id="login-form-username" name="login-form-username" value="" class="form-control" />
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title>Matrix Admin</title><meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="css/matrix-login.css" />
+        <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+
+    </head>
+    <body>
+    <div id="loginbox">
+        <form id="login-form" name="login-form" class="form-vertical" action="index.php" method="post">
+            <div class="control-group normal_text"> <h3><img src="img/logo.png" alt="Logo" /></h3></div>
+            <div class="control-group">
+                <div class="controls">
+                    <div class="main_input_box">
+                        <span class="add-on bg_lg"><i class="icon-user"> </i></span><input type="text" id="login-form-username" name="login-form-username" value="" type="text" placeholder="Username" />
                     </div>
-                    <div class="col_full">
-                        <label for="login-form-password">Password:</label>
-                        <input type="password" id="login-form-password" name="login-form-password" value="" class="form-control" />
-                    </div>
-                    <div class="col_full nobottommargin">
-                        <button class="button button-3d button-black nomargin" id="login-form-submit" name="login-form-submit" value="login">Login</button>
-                        <a href="#" class="fright">Forgot Password?</a>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
+            <div class="control-group">
+                <div class="controls">
+                    <div class="main_input_box">
+                        <span class="add-on bg_ly"><i class="icon-lock"></i></span><input type="password" id="login-form-password" name="login-form-password" value="" type="password" placeholder="Password" />
+                    </div>
+                </div>
+            </div>
+            <div class="form-actions">
+                <span class="pull-left"><a href="#" class="flip-link btn btn-info" id="to-recover">Lost password?</a></span>
+                <span class="pull-right">
+                    <button class="btn btn-success" id="login-form-submit" name="login-form-submit" value="login">Login</button>
+                </span>
+            </div>
+        </form>
+        <form id="recoverform" action="#" class="form-vertical">
+            <p class="normal_text">Enter your e-mail address below and we will send you instructions how to recover a password.</p>
+
+            <div class="controls">
+                <div class="main_input_box">
+                    <span class="add-on bg_lo"><i class="icon-envelope"></i></span><input type="text" placeholder="E-mail address" />
+                </div>
+            </div>
+
+            <div class="form-actions">
+                <span class="pull-left"><a href="#" class="flip-link btn btn-success" id="to-login">&laquo; Back to login</a></span>
+                <span class="pull-right"><a class="btn btn-info"/>Reecover</a></span>
+            </div>
+        </form>
     </div>
 
+    <script src="js/jquery.min.js"></script>
+    <script src="js/matrix.login.js"></script>
+    </body>
+
+    </html>
