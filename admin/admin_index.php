7b272796e751cdcf7a96738debdcf90a0db907cc
Ayham <aylosa@outlook.com>
2017-06-02 17:51:19 +0200

    .

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -94,7 +94,7 @@
               <ul class="site-stats">
                 <li class="bg_lh"><i class="fa fa-users"></i> <strong>2540</strong> <small>Total Users</small></li>
                 <li class="bg_lh"><i class="fa fa-user-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Transactions</small></li>
+                <li class="bg_lh"><i class="fa fa-exchange"></i> <strong>656</strong> <small>Total Transactions</small></li>
                 <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Agents</small></li>
                 <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Delivered Transaction</small></li>
                 <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Pending Transactions</small></li>
