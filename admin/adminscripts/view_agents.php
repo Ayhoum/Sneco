142bb3b08df7a485e0e039c4cbb515bdbbbf9027
Ayham <aylosa@outlook.com>
2017-06-10 16:59:26 +0200

    .

--- admin/adminscripts/view_agents.php
+++ admin/adminscripts/view_agents.php
@@ -6,7 +6,7 @@
 
     <div class="widget-box">
     <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
-        <h5>Transactions</h5>
+        <h5>Agents</h5>
     </div>
     <div class="widget-content nopadding">
     <table class="table table-bordered data-table">
