8dbde1008c96f7bdc642089611d63ccf81dcb6b3
Ayham <aylosa@outlook.com>
2017-06-27 00:35:40 +0200

    .

--- admin/adminscripts/view_users.php
+++ admin/adminscripts/view_users.php
@@ -7,15 +7,14 @@
 
             <div class="widget-box">
                 <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
-                    <h5>Transactions</h5>
+                    <h5>Users</h5>
                 </div>
                 <div class="widget-content nopadding">
                     <table class="table table-bordered data-table">
                         <thead>
                         <tr>
                             <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Email</th>
+                            <th>Name</th>
                             <th>Email</th>
                             <th>Phone</th>
                             <th>Street Name</th>
@@ -26,71 +25,48 @@
                         </thead>
                         <tbody>
                         <?php
-                        $query = "SELECT * FROM TRANSITION ORDER BY ID DESC";
+                        $query = "SELECT * FROM USER ORDER BY ID DESC";
                         $select_posts = mysqli_query($mysqli, $query);
                         while($row = mysqli_fetch_assoc($select_posts)){
                             $id = $row['ID'];
-                            $agent_id = $row['Agent_ID'];
-                            $sender_name = $row['Sender_fName'] . " " . $row['Sender_lName'] ;
-                            $sender_email = $row['Sender_Email'];
-                            $sender_country = $row['Sender_Country'];
-                            $sender_bankaccount = $row['Sender_BankAcount'];
-                            $sender_idnumber = $row['Sender_IdNumber'];
-                            $sender_nationality = $row['Sender_Nationality'];
-                            $sender_expiary = $row['Sender_IdExp'];
-                            $receiver_name = $row['Receiver_fName'] . " " . $row['Receiver_lName'] ;
-                            $receiver_email = $row['Receiver_Email'];
-                            $receiver_country = $row['Receiver_Country'];
-                            $amount = $row['Amount'] . " " . $row['Current_Currency'];
-                            $payment_currency = $row['Payment_Currency'];
-                            $total_amount = $row['Total_Amount'];
-                            $rate = $row['Rate'];
-                            $charge = $row['Charge'];
-                            $reason = $row['Reason'];
-                            $status = $row['Status'];
-                            $timestamp = strtotime($row['Time']);
-                            $date = date('Y-m-d',$timestamp);
-                            $time = date('h:m:s',$timestamp);
+                            $name = $row['First_Name'] . " " . $row['Last_Name'] ;
+                            $email = $row['Email'];
+                            $phone = $row['Phone'];
+                            $streetName = $row['Street_Name'];
+                            $houseName = $row['House_Number'];
+                            $postCode = $row['Postcode'];
+                            $city = $row['City'];
 
                             echo "<tr>";
                             ?>
                             <?php
                             echo "<td>$id</td>";
-                            echo "<td>$sender_name</td>";
-                            echo "<td>$sender_email</td>";
-                            echo "<td>$sender_country</td>";
-                            echo "<td>$sender_bankaccount</td>";
-                            echo "<td>$sender_nationality</td>";
-                            echo "<td>$sender_expiary</td>";
-                            echo "<td>$receiver_name</td>";
-                            echo "<td>$receiver_email</td>";
-                            echo "<td>$receiver_country</td>";
-                            echo "<td>$amount</td>";
-                            echo "<td>$payment_currency</td>";
-                            echo "<td>$total_amount</td>";
-                            echo "<td>$rate</td>";
-                            echo "<td>$charge</td>";
-                            echo "<td>$reason</td>";
-                            echo "<td>$status</td>";
+                            echo "<td>$name</td>";
+                            echo "<td>$email</td>";
+                            echo "<td>$phone</td>";
+                            echo "<td>$streetName</td>";
+                            echo "<td>$houseName</td>";
+                            echo "<td>$postCode</td>";
+                            echo "<td>$city</td>";
 
 
 
-                            echo "<td><a href='transaction.php?delete=$id'>Delete</a></td>";
-                            echo "<td><a href='transaction.php?source=edit_transaction&t_id={$id}'>Edit</a></td>";
+//                            echo "<td><a href='transaction.php?delete=$id'>Delete</a></td>";
+//                            echo "<td><a href='transaction.php?source=edit_transaction&t_id={$id}'>Edit</a></td>";
 
 
-                            $query = "SELECT * FROM AGENT WHERE ID = $agent_id ";
-                            $select_trans_by_id = mysqli_query($mysqli, $query);
-                            while($row = mysqli_fetch_assoc($select_trans_by_id)) {
-
-                                $agent_place = $row['Agent_Area'];
-                            }
-
-                            echo "<td><a href='../../pdf.php?agent_code={$agent_id}&place={$agent_place}&time={$time}&date={$date}&sender_name={$sender_name}&sender_email={$sender_email}'>Download</a></td>";
-
-
-
-                            echo "</tr>";
+//                            $query = "SELECT * FROM AGENT WHERE ID = $agent_id ";
+//                            $select_trans_by_id = mysqli_query($mysqli, $query);
+//                            while($row = mysqli_fetch_assoc($select_trans_by_id)) {
+//
+//                                $agent_place = $row['Agent_Area'];
+//                            }
+//
+//                            echo "<td><a href='../../pdf.php?agent_code={$agent_id}&place={$agent_place}&time={$time}&date={$date}&sender_name={$sender_name}&sender_email={$sender_email}'>Download</a></td>";
+//
+//
+//
+//                            echo "</tr>";
                         }
                         ?>
                         </tbody>
@@ -102,14 +78,14 @@
 </div>
 
 <?php
-
-if(isset($_GET['delete'])){
-
-    $id = $_GET['delete'];
-    $query = "DELETE FROM TRANSITION WHERE ID = {$id} ";
-    $delete_query = mysqli_query($mysqli, $query);
-    header("Location: transaction.php");
-}
+//
+//if(isset($_GET['delete'])){
+//
+//    $id = $_GET['delete'];
+//    $query = "DELETE FROM TRANSITION WHERE ID = {$id} ";
+//    $delete_query = mysqli_query($mysqli, $query);
+//    header("Location: transaction.php");
+//}
 ?>
 
 
