4266c6a90c0102f3e042259045300e0791f617e4
Alaa Semsemea <semsemea.a@gmail.com>
2017-07-15 18:47:52 +0200

    hi

--- admin/agent_index.php
+++ admin/agent_index.php
@@ -6,6 +6,7 @@ date_default_timezone_set('Europe/Amsterdam');
 //include 'rate.php';
 ?>
 <?php
+session_start();
 if(!isset($_SESSION['role'])){
     header("Location: index.php");
 }else if($_SESSION['role'] == "Admin"){
