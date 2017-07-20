cdabe068ce9e4df3ab7999037ed80f5d67753c51
Ayham <aylosa@outlook.com>
2017-07-20 19:42:57 +0200

    .

--- admin/adminscripts/view_invoice_indetails.php
+++ admin/adminscripts/view_invoice_indetails.php
@@ -29,34 +29,19 @@ while($row = mysqli_fetch_assoc($select_posts)) {
     $address3 = $row['address_line3'];
     $discount = $row['discount'];
     $advanced_payment = $row['advanced_payment'];
+    $status = $row['status'];
     $client_id = $row['CLIENT_id'];
-// Invoice Line
-    $query1 = "SELECT * FROM INVOICE_LINE WHERE Invoice_id = '{$invoice_id}' ";
-    $select_posts2 = mysqli_query($mysqli, $query1);
-    while ($row = mysqli_fetch_assoc($select_posts2)) {
-        $item_id = $row['ITEM_id'];
-        $quantity = $row['Quantity'];
-        $total = $row['Total'];
-// Item Name:
-        $query2 = "SELECT * FROM ITEM WHERE id = '{$item_id}' ";
-        $select_posts3 = mysqli_query($mysqli, $query2);
-        while ($row = mysqli_fetch_assoc($select_posts3)) {
-            $item_name = $row['item_name'];
-            $item_price =$row['item_price'];
-            $item_description =$row['item_description'];
 
-// Client Name:
-            $query3 = "SELECT * FROM CLIENT WHERE id = '{$client_id}' ";
-            $select_posts4 = mysqli_query($mysqli, $query3);
-            while ($row = mysqli_fetch_assoc($select_posts4)) {
+    // Client Name:
+    $query3 = "SELECT * FROM CLIENT WHERE id = '{$client_id}' ";
+    $select_posts4 = mysqli_query($mysqli, $query3);
+    while ($row = mysqli_fetch_assoc($select_posts4)) {
 
-                $client_name = $row['Client_name'];
-                $client_address = $row['Client_address_1'] .'<br>'. $row['Client_address_2'];
-                $client_phone = $row['Client_phone'];
-                $client_email = $row['Client_email'];
+        $client_name = $row['Client_name'];
+        $client_address = $row['Client_address_1'] .'<br>'. $row['Client_address_2'];
+        $client_phone = $row['Client_phone'];
+        $client_email = $row['Client_email'];
 
-            }
-        }
     }
 }
 
@@ -83,81 +68,129 @@ while($row = mysqli_fetch_assoc($select_posts)) {
                         <td class="width30"><strong>Shipping Address:</strong></td>
                         <td class="width70"><?php echo $address1."<br>" .$address2. "<br>" .$address3; ?></td>
                     </tr>
+                        <tr>
+                            <td class="width30"><strong>Invoice status</strong></td>
+                            <td class="width70"><?php echo $status; ?></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
             <div class="span6">
                 <table class="table table-bordered table-invoice">
                     <tbody>
-                    <p class="text-center" style="font-size: 20px;"><strong>Item Details</strong></p>
+                    <p class="text-center" style="font-size: 20px;"><strong>Client Details</strong></p>
                     <tr>
-                        <td class="width30"><strong>Item :</strong></td>
-                        <td class="width70"><?php echo $item_name; ?></td>
+                        <td class="width30"><strong>Client :</strong></td>
+                        <td class="width70"><?php echo $client_name; ?></td>
                     </tr>
                     <tr>
-                        <td class="width30"><strong>Price:</strong></td>
-                        <td class="width70"><?php echo $item_price; ?></td>
+                        <td class="width30"><strong>Main Address :</strong></td>
+                        <td class="width70"><?php echo $client_address; ?></td>
                     </tr>
                     <tr>
-                        <td class="width30"><strong>Description:</strong></td>
-                        <td class="width70"><?php echo $item_description; ?></td>
+                        <td class="width30"><strong>Email:</strong></td>
+                        <td class="width70"><?php echo $client_email; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="width30"><strong>Phone:</strong></td>
+                        <td class="width70"><?php echo $client_phone; ?></td>
                     </tr>
                     </tbody>
                 </table>
             </div>
         </div>
         <br>
-        <div class="span6">
-            <table class="table table-bordered table-invoice">
-                <tbody>
-                <p class="text-center" style="font-size: 20px;"><strong>Client Details</strong></p>
-                <tr>
-                    <td class="width30"><strong>Client :</strong></td>
-                    <td class="width70"><?php echo $client_name; ?></td>
-                </tr>
-                <tr>
-                    <td class="width30"><strong>Main Address :</strong></td>
-                    <td class="width70"><?php echo $client_address; ?></td>
-                </tr>
-                <tr>
-                    <td class="width30"><strong>Email:</strong></td>
-                    <td class="width70"><?php echo $client_email; ?></td>
-                </tr>
-                <tr>
-                    <td class="width30"><strong>Phone:</strong></td>
-                    <td class="width70"><?php echo $client_phone; ?></td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-    </div>
         <div class="row-fluid">
+
+        <div class="span12">
+            <div class="widget-content nopadding">
+                <table class="table table-bordered data-table">
+                    <thead>
+                    <tr>
+
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Total per item</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $invoice_total_amount = 0;
+                    $query1 = "SELECT * FROM INVOICE_LINE WHERE Invoice_id = '{$invoice_id}' ";
+                        $select_posts2 = mysqli_query($mysqli, $query1);
+                        if (mysqli_num_rows($select_posts2) > 0 ) {
+                            while ($row = mysqli_fetch_assoc($select_posts2)) {
+                                $item_id = $row['ITEM_id'];
+                                $quantity = $row['Quantity'];
+                                $total = $row['Total'];
+
+
+                                // Item Name:
+                                $query2 = "SELECT * FROM ITEM WHERE id = '{$item_id}' ";
+                                $select_posts3 = mysqli_query($mysqli, $query2);
+                                while ($row = mysqli_fetch_assoc($select_posts3)) {
+                                    $item_name = $row['item_name'];
+                                    $item_description = $row['item_description'];
+                                    $item_price = $row['item_price'];
+                                    $total_item_price = $item_price * $quantity;
+                                    echo "<tr>";
+
+                                    echo "<td>$item_name ($item_description)</td>";
+                                    echo "<td>$quantity</td>";
+                                    echo "<td>$total_item_price</td>";
+
+                                    echo "</tr>";
+                                    $invoice_total_amount = $invoice_total_amount + $total_item_price;
+                                }
+                            }
+
+                        }
+
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+        <br>
+        <br>
+        <br>
+    <div class="row-fluid">
             <div class="span12">
-                <table class="table table-bordered table-invoice-full">
+                <table class="table table-bordered">
                     <tbody>
                     <tr>
                         <td class="msg-invoice" width="85%"><h4>Payments</h4>
 <!--                            <a href="#" class="tip-bottom" title="Wire Transfer">Wire transfer</a> |  <a href="#" class="tip-bottom" title="Bank account">Bank account #</a> |  <a href="#" class="tip-bottom" title="SWIFT code">SWIFT code </a>|  <a href="#" class="tip-bottom" title="IBAN Billing address">IBAN Billing address </a></td>-->
                         <td class="right">
-                            <strong>1 * Item Price :</strong> <br>
-                            <strong>Quantity</strong> <br>
+                            <strong>Total invoice amount :</strong> <br>
                             <strong>Discount</strong> <br>
                             <strong>Advanced Payment</strong> <br>
                             <strong> </strong> <br>
                             </td>
-                        <td class="right"><strong><?php echo $item_price; ?><br>
-                                <?php echo $quantity; ?> <br>
+                        <td class="right"><strong><?php echo $invoice_total_amount; ?><br>
                                 <?php echo $discount ?> <br>
                                 <?php echo $advanced_payment; ?> <br></strong></td>
                     </tr>
                     </tbody>
                 </table>
+                <?php
+                $invoice_total_amount = $invoice_total_amount - $discount - $advanced_payment;
+                ?>
                 <div class="pull-right">
-                    <h4><span>Total:</span> <?php echo $total?></h4>
+                    <h4><span>Total to pay:</span> <?php if($invoice_total_amount == 0){echo "Done!";}else{echo $invoice_total_amount;} ?></h4>
                     <br>
-                    <a class="btn btn-primary btn-large pull-right" href="send_email.php?">Send to agent</a></div>
+                    <a class="btn btn-success btn-large pull-right" href="view_invoice_indetails.php?approve=<?php echo $invoice_id; ?>">Approve</a></div>
             </div>
         </div>
     </div>
 </div>
+<?php
+if(isset($_GET['approve'])){
+    $id = $_GET['approve'];
+    $query = "UPDATE INVOICE SET status = 'Approved' WHERE ID = {$id}";
+    $block_agent_query = mysqli_query($mysqli, $query);
+    header("Location: invoice_indetails.php?i_id={$id}");
+}
+?>
